14d88fbbeaf1b2e2bd5351b55b1c5f3a219094d1
Mark S <markscicluna@hotmail.com>
2018-12-21 15:50:42 +0000

    added istanbul and qunitjs for testing and test coverage

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
+/* The code block below ONLY Applies to Node.js - This Demonstrates
+   re-useability of JS code in both Back-end and Front-end! #isomorphic */
+/* istanbul ignore if */
+if (typeof module !== 'undefined' && module.exports) {
+  var QUnit = require('qunitjs'); // require QUnit node.js module
+  // alias the QUnit.test method so we don't have to change all our tests
+  var test = QUnit.test; // stores a copy of QUnit.test
+  require('qunit-tap')(QUnit, console.log); // use console.log for test output
+  var getChange = require('./change.js'); // load our getChange method
+}
+
 test('getChange(1,1) should equal [] - an empty array', function(assert) {
   var result = getChange(1, 1); //no change/coins just an empty array
   var expected = [];
@@ -13,3 +24,6 @@ test('getChange(486, 600) should equal [100, 10, 2, 2]', function(assert) {
   var expected = [100, 10, 2, 2];
   assert.deepEqual(result, expected);
 });
+
+/* istanbul ignore next */
+if (typeof module !== 'undefined' && module.exports) { QUnit.load(); } // run the tests
